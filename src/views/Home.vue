6ae4b30b5b4d4932c9e717699cddc06488336e8c
Luke <Lukegarrigan8@gmail.com>
2020-10-22 08:45:40 +0100

    I keep losing to my mum in scrabble.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+    <input class="input" v-model="letters" maxlength="10">
+    <HelloWorld :letters="letters"/>
   </div>
 </template>
 
@@ -14,5 +16,26 @@ import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
     HelloWorld
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  public letters: string = '';
+
+
+}
 </script>
+
+
+<style> 
+
+body {
+  background: #42b983;
+}
+input { 
+  display: flex;
+  flex: 1;
+  width: 300px;
+  height: 50px;
+  margin: auto;
+  font-size: 30px;
+}
+
+</style>
